8f9e8636f517695d015fb39e0c6e8a2638bea3a1
Pratishtha003 <149357686+Pratishtha003@users.noreply.github.com>
2024-05-13 23:11:16 +0530

    Add files via upload

---
+++ new-car-2/React-Car-Rental-Website-master/src/assets/data/serviceData.js
@@ -0,0 +1,44 @@
+const serviceData = [
+  {
+    id: 1,
+    title: "abc",
+    icon: "ri-map-pin-2-line",
+    desc: " Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit ",
+  },
+  {
+    id: 2,
+    title: "abc",
+    icon: "ri-community-line",
+    desc: "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit",
+  },
+
+  {
+    id: 3,
+    title: "abc",
+    icon: "ri-roadster-line",
+    desc: "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit",
+  },
+
+  {
+    id: 4,
+    title: "abc",
+    icon: "ri-timer-flash-line",
+    desc: "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit",
+  },
+
+  {
+    id: 5,
+    title: "abc",
+    icon: "ri-map-pin-line",
+    desc: "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit",
+  },
+
+  {
+    id: 6,
+    title: "abc",
+    icon: "ri-flight-takeoff-line",
+    desc: "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit",
+  },
+];
+
+export default serviceData;
